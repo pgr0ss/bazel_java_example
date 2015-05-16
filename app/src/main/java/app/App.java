f9f4976e9096e8e5c27ac42b9361194ad69a88fd
Paul Gross <pgross@gmail.com>
2015-05-15 22:10:18 -0700

    Add a more complicated test.

--- app/src/main/java/app/App.java
+++ app/src/main/java/app/App.java
@@ -3,9 +3,19 @@ package app;
 import persistence.Repository;
 
 public class App {
-    public String go() {
-        Repository repository = new Repository();
+
+    private Repository repository;
+
+    public App() {
+        this.repository = new Repository();
         repository.set("message", "go go go");
+    }
+
+    public App(Repository repository) {
+        this.repository = repository;
+    }
+
+    public String go() {
         return repository.get("message");
     }
 
--- app/src/test/java/app/AppTest.java
+++ app/src/test/java/app/AppTest.java
@@ -1,14 +1,23 @@
 package app;
 
 import org.junit.Test;
+import persistence.Repository;
 
 import static org.junit.Assert.assertEquals;
 
 public class AppTest {
 
     @Test
-    public void testApp() {
+    public void testAppDefault() {
         App app = new App();
         assertEquals("go go go", app.go());
+    }
+
+    @Test
+    public void testAppWithSpecificMessage() {
+        Repository repository = new Repository();
+        repository.set("message", "stop stop stop");
+        App app = new App(repository);
+        assertEquals("stop stop stop", app.go());
     }
 }
